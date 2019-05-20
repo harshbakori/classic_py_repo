897ecdbbe89fdf964c2024eaee806126fd2efa47
D3@D53C71 <30556333+harshbakori@users.noreply.github.com>
2019-05-20 11:02:43 +0530

    as

--- opencv/video_face_ditect.py
+++ opencv/video_face_ditect.py
@@ -1,6 +1,7 @@
 #capturing video
 import cv2,time
-face_cascade = cv2.CascadeClassifier("H:\\Github\\semester 5 projects\\py\\opencv\\opencv-master\\data\\haarcascades\\haarcascade_frontalface_alt2.xml")#xmlfile having face specifications
+#face_cascade = cv2.CascadeClassifier("H:\\Github\\semester 5 projects\\py\\opencv\\opencv-master\\data\\haarcascades\\haarcascade_frontalface_alt.xml")#xmlfile having face specifications
+face_cascade = cv2.CascadeClassifier("/media/harsh/Dead Zone {-_-}/Github/semester 5 projects/py/opencv/opencv-master/data/haarcascades/haarcascade_frontalface_alt.xml")#xmlfile having face specifications
 
 video = cv2.VideoCapture(0)
 
@@ -13,10 +14,10 @@ while True:
     check,frame = video.read()
     gray_img=cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY) #converting to grayscale img
     
-    faces = face_cascade.detectMultiScale(gray_img,scaleFactor = 1.2,minNeighbors=5)#saving img with face ditect file 
+    faces = face_cascade.detectMultiScale(gray_img,scaleFactor = 1.2,minNeighbors=5) #saving img with face ditect file 
     for x,y,w,h in faces: #for each face in faces object
         frame=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)#make rectangal to detect
-
+       # print(x,x+w,y+h,y)
     
     cv2.imshow("capture",frame)
 
